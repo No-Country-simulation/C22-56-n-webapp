4f11cca06460a3ec843014291eb52ee5a0df8521
ns <clidertutayarivera@gmail.com>
2024-11-19 21:53:12 -0500

    basico

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import mainRoutes from './routes/mainRoutes';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000
+
+//Principal
+app.use('/', (req, res) => {
+    res.send('Welcome to TEAM C22-56-N-WEBAPP API')
+})
+
+//rutas
+app.use('/api', mainRoutes)
+
+// Manejador de errores globales
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Algo salió mal!' });
+});
+
+//Correr en el puerto
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto: ${PORT}`)
+})
---
+++ server/routes/mainRoutes.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const mainRoutes = Router()
+
+export default mainRoutes
