be8065ea71b40d371ad05cd443189d2f7411eef4
marbello1973 <david.marbello@outlook.com>
2024-11-21 21:01:29 -0500

    carga dinamica de los modelos en mongo

--- server/models/ProductoModel.js
+++ server/models/ProductoModel.js
@@ -1,1 +1,14 @@
-//David
+import mongoose from "mongoose";
+
+const ProductoSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    descripcion: { type: String, required: true },
+    precio: { type: Number, required: true },
+    cantidad: { type: Number, required: true },
+    categoria: { type: String, required: true },    
+    fechaCreacion: { type: Date, default: Date.now },
+});
+
+const ProductoModel = mongoose.model('Producto', ProductoSchema);
+export default ProductoModel;
+
