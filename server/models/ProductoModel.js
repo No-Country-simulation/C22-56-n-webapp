120c96084034adf3905c0b9e50d2229ff09c46cd
PHRCOOK <pabloelleproso@gmail.com>
2024-12-03 20:46:23 -0300

    creacion de componentes

--- server/models/ProductoModel.js
+++ server/models/ProductoModel.js
@@ -5,32 +5,32 @@ const ProductoSchema = new mongoose.Schema({
   nombre: {
     type: String,
     required: [true, "El nombre del producto es obligatorio."],
-    trim: true, // Elimina espacios adicionales al principio o final
+    trim: true,
   },
   descripcion: {
     type: String,
     required: [true, "La descripción del producto es obligatoria."],
-    trim: true, // Elimina espacios adicionales al principio o final
+    trim: true,
   },
   precio: {
     type: Number,
     required: [true, "El precio del producto es obligatorio."],
-    min: [0, "El precio no puede ser negativo."], // Validación para evitar precios negativos
+    min: [0, "El precio no puede ser negativo."],
   },
   cantidad: {
     type: Number,
     required: [true, "La cantidad del producto es obligatoria."],
-    min: [0, "La cantidad no puede ser negativa."], // Validación para evitar cantidades negativas
+    min: [0, "La cantidad no puede ser negativa."],
   },
   categoria: {
     type: String,
     required: [true, "La categoría del producto es obligatoria."],
-    enum: ["Electrónica", "Ropa", "Hogar", "Alimentos", "Juguetes"], // Validación para limitar las categorías
-    trim: true, // Elimina espacios adicionales al principio o final
+    enum: ["Electrónica", "Ropa", "Hogar", "Alimentos", "Juguetes"],
+    trim: true,
   },
   fechaCreacion: {
     type: Date,
-    default: Date.now, // Valor predeterminado es la fecha y hora actual
+    default: Date.now,
   },
   historialPrecios: [
     {
@@ -47,31 +47,23 @@ const ProductoSchema = new mongoose.Schema({
   ],
 });
 
-// Añadir un índice para mejorar el rendimiento de las consultas por categoría y nombre
+// Índices para mejorar consultas
 ProductoSchema.index({ categoria: 1, nombre: 1 });
 
-// Pre-save hook para validar que la cantidad y el precio estén correctos antes de guardar el producto
+// Middleware para manejar actualizaciones
 ProductoSchema.pre("save", function (next) {
-  if (this.precio < 0 || this.cantidad < 0) {
-    return next(
-      new Error("El precio y la cantidad deben ser números positivos.")
-    );
+  if (this.isModified("precio")) {
+    this.historialPrecios.push({ precio: this.precio });
   }
-
-  // Registrar el precio en el historial de precios antes de guardar el producto
-  this.historialPrecios.push({
-    precio: this.precio,
-  });
-
   next();
 });
 
-// Método para calcular el valor total del producto
+// Método para calcular el valor total
 ProductoSchema.methods.calcularValorTotal = function () {
   return this.precio * this.cantidad;
 };
 
-// Crear y exportar el modelo
+// Exportar el modelo
 const ProductoModel = mongoose.model("Producto", ProductoSchema);
 
 export default ProductoModel;
